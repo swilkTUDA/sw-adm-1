f826aa5815c566ef8bb91fdf5286719112f2ed3f
swilkTUDA <swilkTUDA@users.noreply.github.com>
2024-09-24 21:22:20 +0000

    initial running commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import requests
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+url = "https://peerberryinvestsw.azurewebsites.net/api/peerberryinvesthttptrigger?code="
+st.title("SW Admin Panel for Peerberry")
+tfa = st.text_input("Two Factor Code", "000000")
+if st.button("Starte Peerberry"):
+    pb_payload = {
+        "tfaCode": tfa
+    }
+    response = requests.post(url + os.getenv("PEERBERRY_FUNCTION_API_KEY"), json=pb_payload)
+    
+    st.write("Daten übertragen: " + str(response))
