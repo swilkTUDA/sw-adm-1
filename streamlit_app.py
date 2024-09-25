d930220033e4c7104c2173c73e07bd0a2ac071ce
swilkTUDA <swilkTUDA@users.noreply.github.com>
2024-09-25 07:28:59 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,30 @@
 import streamlit as st
 import requests
 import os
+import json
 
-url = "https://peerberryinvestsw.azurewebsites.net/api/peerberryinvesthttptrigger?code="
-api_key = st.secrets["PEERBERRY_FUNCTION_API_KEY"]
+url = "https://peerberryinvestsw.azurewebsites.net/api/peerberry_auth?code="
+api_key = st.secrets["PEERBERRY_FUNCTION_AUTH_API_KEY"]
 
 st.title("SW Admin Panel for Peerberry")
-tfa = st.text_input("Two Factor Code", "000000")
+tfa = st.text_input("Two Factor Code", "")
 if st.button("Starte Peerberry"):
     pb_payload = {
         "tfaCode": tfa
     }
     response = requests.post(url + api_key, json=pb_payload)
-    
-    st.write("Daten übertragen: " + str(response))
+    if response.status_code == 200:
+        access_key = response.get("access_key")
+        iteration = 0
+        while iteration < 12:
+            url = "https://peerberryinvestsw.azurewebsites.net/api/peerberry_invest?code="
+            api_key = st.secrets["PEERBERRY_FUNCTION_INVEST_API_KEY"]
+            pb_payload = {
+                "access_key": access_key
+            }
+            response = requests.post(url + api_key, json=pb_payload)
+            st.write("Iteration "+str(iteration)+" completed!" )
+            st.write("Result of the iteration: "+json.dumps(response))
+            iteration += 1
+    else:
+        st.write("Access Key nicht erhalten: FEHLER in AUTH!" )
