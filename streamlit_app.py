56bc197f4d6934dfb5cb4555fd85b3784f3bc349
swilkTUDA <swilkTUDA@users.noreply.github.com>
2024-09-25 07:28:36 +0000

    Added loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,14 @@ import requests
 import os
 
 url = "https://peerberryinvestsw.azurewebsites.net/api/peerberryinvesthttptrigger?code="
+api_key = st.secrets["PEERBERRY_FUNCTION_API_KEY"]
+
 st.title("SW Admin Panel for Peerberry")
 tfa = st.text_input("Two Factor Code", "000000")
 if st.button("Starte Peerberry"):
     pb_payload = {
         "tfaCode": tfa
     }
-    response = requests.post(url + os.getenv("PEERBERRY_FUNCTION_API_KEY"), json=pb_payload)
+    response = requests.post(url + api_key, json=pb_payload)
     
     st.write("Daten übertragen: " + str(response))
