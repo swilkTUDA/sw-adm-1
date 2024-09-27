a6946c7b87010382fa2e100270056403620a8053
swilkTUDA <swilkTUDA@users.noreply.github.com>
2024-09-27 21:32:06 +0000

    gogo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,40 @@ import streamlit as st
 import requests
 import json
 from azure.storage.queue import QueueClient, TextBase64EncodePolicy
+import time
 
 url = st.secrets["PEERBERRY_FUNCTION_AUTH_URL"]
 api_key = st.secrets["PEERBERRY_FUNCTION_AUTH_API_KEY"]
 
 st.title("SW Admin Panel for Peerberry")
 tfa = st.text_input("Two Factor Code", "")
+
+if st.button("Frage verfügbares Budget ab"):
+    pb_payload = {
+        "tfaCode": tfa
+    }
+    response = requests.post(url + api_key, json=pb_payload)
+    if response.status_code == 200:
+        access_key = response.json().get("access_token")
+        iteration = 0
+        while iteration < 12:
+            url = st.secrets["PEERBERRY_FUNCTION_BUDGET_URL"]
+            api_key = st.secrets["PEERBERRY_FUNCTION_BUDGET_API_KEY"]
+            pb_payload = {
+                "access_token": access_key
+            }
+            response = requests.post(url + api_key, json=pb_payload)
+            if response.status_code == 200:
+                st.subheader("Budget [€]", divider=True)
+                st.write(json.dumps(response.json()))
+                time.sleep(15)
+            else:
+                st.write("Error in budget request: "+str(response))
+                break
+            iteration += 1
+    else:
+        st.write("Access Key nicht erhalten: FEHLER in AUTH!" )
+
 if st.button("Starte Peerberry"):
     pb_payload = {
         "tfaCode": tfa
@@ -33,7 +61,7 @@ if st.button("Starte Peerberry"):
     else:
         st.write("Access Key nicht erhalten: FEHLER in AUTH!" )
     
-    # ---------------------
+# ---------------------
 if st.button("Starte Peerberry (Queue)"):
     pb_payload = {
         "tfaCode": tfa
