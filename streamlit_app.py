a46cad32ccda047a73f7bda907283ee030f7a916
swilkTUDA <swilkTUDA@users.noreply.github.com>
2024-11-16 20:34:27 +0000

    adding plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import time
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
+from azure.data.tables import TableServiceClient
 
 
 url = st.secrets["PEERBERRY_FUNCTION_AUTH_URL"]
@@ -76,4 +77,58 @@ with tab1:
         else:
             st.write("Access Key nicht erhalten: FEHLER in AUTH!" )
 with tab2:
-    st.write("Not implemented yet")
+    AZURE_STORAGE_CONNECTION_STRING = st.secrets["HAUSHALTSBUCH_TABLE_CONNECTION"]
+    TABLE_NAME = st.secrets["HAUSHALTSBUCH_TABLE_NAME"]
+
+    service = TableServiceClient.from_connection_string(conn_str=AZURE_STORAGE_CONNECTION_STRING)
+    table_client = service.get_table_client(table_name=TABLE_NAME)
+    entities = table_client.list_entities()
+    data = []
+
+    for entity in entities:
+        if  "-" in entity["Datum"] and entity["EigeneKategorie"] not in ['Umbuchung', 'Kreditkarte']:
+            entity['Betrag'] = entity['Betrag'].replace(",",".")
+            data.append(entity)
+
+    df = pd.DataFrame(data)
+    df['Datum'] = pd.to_datetime(df['Datum'], format='%Y-%m-%d %H:%M:%S') 
+    df['Betrag'] = pd.to_numeric(df['Betrag'])  # Ensure 'Betrag' is numeric
+
+    st.sidebar.title("Filter")
+    latest_date = df['Datum'].max()
+    default_year = latest_date.year
+    default_month = latest_date.month
+    selected_year = st.sidebar.selectbox("Select Year", df['Datum'].dt.year.unique(),index=list(df['Datum'].dt.year.unique()).index(default_year))
+    selected_month = st.sidebar.selectbox("Select Month", df['Datum'].dt.month.unique(),index=list(df['Datum'].dt.month.unique()).index(default_month))
+
+    filtered_df = df[(df['Datum'].dt.year == selected_year) & (df['Datum'].dt.month == selected_month)]
+    # Calculate the sum of expenses and income
+    total_expenses = filtered_df[filtered_df['Betrag'] < 0]['Betrag'].sum()
+    total_income = filtered_df[filtered_df['Betrag'] > 0]['Betrag'].sum()
+
+    # Display the sums
+    st.write(f"Total Expenses: {total_expenses:.2f}")
+    st.write(f"Total Income: {total_income:.2f}")
+    
+    st.title(f"Expenses per Category for {selected_year}-{selected_month:02d}")
+    expenses = filtered_df[(filtered_df['Betrag'] < 0) & (df['EigeneKategorie'] != 'Einnahme')]
+    category_grouped = expenses.groupby('EigeneKategorie').agg({'Betrag': 'sum'}).reset_index()
+    category_grouped = category_grouped.sort_values(by='Betrag', ascending=False)
+    # Plotting with Matplotlib
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = ax.bar(category_grouped['EigeneKategorie'], category_grouped['Betrag'], color='skyblue')
+
+    ax.set_xlabel('Category')
+    ax.set_ylabel('Total Expenses')
+    ax.set_title(f"Expenses per Category for {selected_year}-{selected_month:02d}")
+
+    # Adding the labels
+    for bar in bars:
+        yval = bar.get_height()  # Get the y value of the bar
+        ax.text(bar.get_x() + bar.get_width()/2, yval, round((-1.0)*yval, -1), ha='center', va='bottom')
+
+    plt.xticks(rotation=45)  # Rotate category labels for better readability
+    plt.tight_layout()  # Adjust layout to make room for the labels
+
+    # Streamlit command to display the plot
+    st.pyplot(fig)
